6c9b70d99fb99f466f76114f7aab767ce734ff74
KyahJemz <jameslayson.0@gmail.com>
2024-08-29 23:48:24 +0800

    re compile

--- src/components/EditorJs/Editor.tsx
+++ src/components/EditorJs/Editor.tsx
@@ -1,33 +1,32 @@
 // @ts-nocheck
-import React, { useEffect, useRef } from 'react';
-import EditorJS, { EditorConfig } from '@editorjs/editorjs';
-import { EDITOR_TOOLS } from './EditorTools';
+import React, { useEffect, useRef } from "react";
+import EditorJS, { OutputData } from "@editorjs/editorjs";
+import { EDITOR_TOOLS } from "./EditorTools";
 
-const Editor = ({ data, onChange, holder }) => {
-	const ref = useRef(null);
+export default function Editor({ data, onChange, holder }) {
+  const ref = useRef();
 
-	useEffect(() => {
-		if (!ref.current) {
-			const editor = new EditorJS({
-				holder: holder,
-				tools: EDITOR_TOOLS,
-				data,
-				async onChange(api) {
-					const outputData = await api.saver.save();
-					onChange(outputData);
-				},
-			});
-			ref.current = editor;
-		}
+  useEffect(() => {
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: holder,
+        tools: EDITOR_TOOLS,
+        data,
+        async onChange(api, event) {
+          const data = await api.saver.save();
+          onChange(data);
+        },
+      });
+      ref.current = editor;
+    }
 
-		return () => {
-			if (ref.current && ref.current.destroy) {
-				ref.current.destroy();
-			}
-		};
-	}, [data, onChange, holder]);
+    return () => {
+      if (ref.current && ref.current.destroy) {
+        ref.current.destroy();
+      }
+    };
+  }, []);
 
-	return <div id={holder}></div>;
+
+  return <div id={holder} className="prose max-w-full" />;
 };
-
-export default Editor;
